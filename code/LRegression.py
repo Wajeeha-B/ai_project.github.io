fb362ff247844995da578b41f8e4e75fe6e3e29c
johneh-1 <john.e.hunter-1@student.uts.edu.au>
2024-05-02 10:24:10 +1000

    linear_regression_implementation

--- code/LRegression.py
+++ code/LRegression.py
@@ -1,1 +1,40 @@
 # Contributions - John
+import numpy as np
+from sklearn import metrics
+class LRegression:
+    def __init__(self):
+        pass
+
+    def LinReg(self,features,target):
+        # Features to be used for regression
+        self.features = features
+
+        # Target for regression
+        self.target = target
+
+        # Assign inputs for calculation
+        x1 = self.features
+        y = self.target
+
+        # Calculate weights
+        self.w = np.linalg.inv(np.transpose(x1) @ x1) @ x1.T @ y
+
+        # Make prediction
+        y_pred = x1 @ self.w
+        self.y_pred = y_pred.reshape(-1,1)
+
+        # Return prediction
+        return self.y_pred
+    
+    def MSE(self):
+        # Target value
+        self.target
+
+        # Predicted value
+        self.y_pred
+
+        # Calculate MSE
+        self.y_mse = metrics.mean_squared_error(self.target,self.y_pred)
+
+        # Return MSE
+        return self.y_mse
