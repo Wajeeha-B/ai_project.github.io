d6b3448a4a116117fca941f155387a995f563303
TheKul1 <awlp8402@gmail.com>
2024-05-08 15:09:18 +1000

    fixed kmeans

--- code/kmeans.py
+++ code/kmeans.py
@@ -14,8 +14,20 @@ class Kmeans:
     self.rng = np.random.default_rng(10)  # the most random number/ the constructor for creating random numbers
 
   def cluster(self):
-    x_column = np.array(self.x[self.pref])
+    if self.pref == 'Type':
+      mapping = {'h': 0, 'u': 1}
+
+      x_column = self.x[self.pref].copy()
+
+      for key, value in mapping.items():
+        x_column.replace(key, value, inplace=True)
+
+      x_column = np.array(x_column)
+
+    else:
+      x_column = np.array(self.x[self.pref])
     #   print(x_column)
+    
 
     y_column = np.array(self.y)
     #   print(y_column)
@@ -27,12 +39,10 @@ class Kmeans:
     # x_new['PointsY'] = points[:,1]
 
     # Print the shape of the resulting array
-    print(points.shape)
-    # k = 10
+    # print(points.shape)
 
     centroids = self.rng.choice(points, size=self.k, replace=False)
     # print("Initial centroids:", centroids)
-
     assignment = np.zeros(len(points), dtype=int)
 
     assignment_prev = None
@@ -49,11 +59,14 @@ class Kmeans:
         centroids[i] = points[assignment==i].mean(axis=0)
       
     # print("Final centroids:", centroids)
-    x_new['Assignment'] = assignment
+    x_new['Assignment' + self.pref] = assignment
     return x_new, points, centroids
 
 
-  def plotKmean(x, points, centroids):
-    plt.scatter(points[:,0], points[:,1], c=x['Assignment'],cmap='Set3')
+  def plotKmean(self, x, points, centroids, pref):
+    plt.scatter(points[:,0], points[:,1], c=x['Assignment' + pref],cmap='Set3')
     plt.scatter(centroids[:,0], centroids[:,1], c='k', marker='*')
+    plt.xlabel(pref)  # Set label for x-axis
+    plt.ylabel('Price')  # Set label for y-axis
+    plt.title('Price Vs ' + self.pref)  # Set title for the plot
     plt.show()
