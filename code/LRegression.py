ecf7de712dd81c830174041790f2745cdc2cdec9
johneh-1 <john.e.hunter-1@student.uts.edu.au>
2024-05-12 10:18:37 +1000

    linear regression completion

--- code/LRegression.py
+++ code/LRegression.py
@@ -8,29 +8,26 @@ class LRegression:
 
     def LinReg(self,x,y,values,assignments,k):
         self.w = []
-        i = 0
-        j = 0
-        for value in values:
-            i = i + 1
-            for assignment in assignments:
-                j = j + 1
-                if i == j:
-                    x1 = x.loc[:,value][x[assignment] == k]
-                    y1 = y[x[assignment] == k]
-                    x1 = x1.values.reshape(-1,1)
-                    y1 = y1.values.reshape(-1,1)
-                    x1t = np.transpose(x1)
-                    x1tx = np.dot(x1t,x1)
-                    x1tx_inv = np.linalg.inv(x1tx)
-                    x1ty = np.dot(x1t,y1)
-                    weight = np.dot(x1tx_inv,x1ty)
-                    self.w.append(weight)
-                elif i != j:
-                    j = i
+        for value,assignment in zip(values,assignments):
+            if value not in x.columns or assignment not in x.columns:
+                continue
+            print("value = ",value,"assignment = ",assignment)
+            x1 = x.loc[:,value][x[assignment] == k]
+            y1 = y[x[assignment] == k]
+            x1 = x1.values.reshape(-1,1)
+            y1 = y1.values.reshape(-1,1)
+            x1t = np.transpose(x1)
+            x1tx = np.dot(x1t,x1)
+            x1tx_inv = np.linalg.inv(x1tx)
+            x1ty = np.dot(x1t,y1)
+            weight = np.dot(x1tx_inv,x1ty)
+            self.w.append(weight)
         return self.w
     
     def MSE(self,x,w,y):
         y_pred = x @ w
+
+        # y_pred = y_pred / w.shape[1]
         # Calculate MSE
         self.y_mse = metrics.mean_squared_error(y,y_pred)
 
